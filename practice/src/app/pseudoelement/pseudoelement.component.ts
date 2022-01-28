2aed1e01ee43f77b0d2198559579c71a2eecabff
Khyatipate <khyati1502@gmail.com>
2022-01-28 15:02:07 +0530

    add pseudo class and element

--- practice/src/app/pseudoelement/pseudoelement.component.ts
+++ practice/src/app/pseudoelement/pseudoelement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-pseudoelement',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pseudoelement.component.scss']
 })
 export class PseudoelementComponent implements OnInit {
+  title = 'Routing_Example';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  signupForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]{2,}')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [Validators.required, Validators.maxLength(3)]),
+    gender: new FormControl('', Validators.required)
+  })
+  userSignUp() {
+    console.log(this.signupForm);
 
+  }
+  get user() {
+    return this.signupForm.get('name');
+
+  }
+  get email() {
+    return this.signupForm.get('email');
+
+  }
+  get age() {
+    return this.signupForm.get('age')
+  }
 }
+
